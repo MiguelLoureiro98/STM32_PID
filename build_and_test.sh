d7fe3f9c11a72dfb6221aea75f2d89ac7c923152
Miguel Loureiro <miguel.santos.loureiro@gmail.com>
2025-06-12 19:56:45 +0100

    Added argument checking to shell script.

--- build_and_test.sh
+++ build_and_test.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-cmake -S . -B build/
+build_type=${1:-"Debug"}
+
+if ([ $build_type != "Debug" ] && [ $build_type != "Release" ])
+then
+
+    echo "Error: $build_type unrecognised. Please select either Debug or Release as the build type."
+    exit 1
+
+fi
+
+echo "Build type: $build_type"
+
+cmake -DCMAKE_BUILD_TYPE=$build_type -S . -B build/
 cd build
 make
 ./test_lib --verbose
