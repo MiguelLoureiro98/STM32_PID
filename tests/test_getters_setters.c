a49676fceb8aef852a766e07d416bf9e2ba1e656
Miguel Loureiro <miguel.santos.loureiro@gmail.com>
2025-06-17 15:56:07 +0100

    Added init tests.

--- tests/test_getters_setters.c
+++ tests/test_getters_setters.c
@@ -9,6 +9,12 @@ Test(getset_tests, init){
 
     PID_init(&controller, 0.0, 0.0, 0.0, 0.0, -1.0, 1.0, 1.0);
 
-    cr_assert(get_Kp(&controller) == 0.0, "Failed to initialise Kp.");
+    cr_assert(get_Kp(&controller) == 0.0, "Failed to initialise Kp.\n");
+    cr_assert(get_Ki(&controller) == 0.0, "Failed to initialise Ki.\n");
+    cr_assert(get_Kd(&controller) == 0.0, "Failed to initialise Kd.\n");
+    cr_assert(controller.tau == 0.0, "Failed to initialise tau.\n");
+    cr_assert(controller.Ts == 1.0, "Failed to initialise Ts.\n");
+    cr_assert(controller.umin == -1.0, "Failed to initialise umin.\n");
+    cr_assert(controller.umax == 1.0, "Failed to initialise umax.");
 
 }
